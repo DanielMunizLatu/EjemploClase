fe8aa4d2cde08ef20d43b20f728640d378e6396c
DanielMuniz <dmunizv@gmail.com>
2023-08-14 21:04:15 -0300

    commit otro branch

--- EjemploGit/src/presentacion/Clase2.java
+++ EjemploGit/src/presentacion/Clase2.java
@@ -4,7 +4,7 @@ public class Clase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.print("Hola pepe")
+        System.out.print("Hola pepe");
 	}
 
 }
